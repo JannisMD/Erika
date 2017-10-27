65e196505ce88cdfbb9d18f6f8bba3b2699fdd23
JannisMD <1611773@stud.hs-mannheim.de>
2017-10-27 13:49:09 +0200

    Kleine Änderung an Nachricht senden der Hauptseite

--- websites/assets/js/main.js
+++ websites/assets/js/main.js
@@ -164,3 +164,22 @@
 	});
 
 })(jQuery);
+
+function sendData(form){
+
+	if(true){
+		console.log("true");
+		var formData = new FormData(form);
+
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', 'http://localhost:3000/nachricht' ,true);
+		xhr.responseTyp = 'json';
+		xhr.onload = function(json){ }
+		xhr.send(formData);
+		console.log("abgeschickt");
+		alert("Ihre Daten wurden erfolgreich an den Server übermittelt");
+
+	}	else {
+		console.log("false");
+	  }
+}
