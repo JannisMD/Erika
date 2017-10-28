cb301f5150ec3bf7cb15794300902759631a1dec
SefaKaya <1526952@stud.hs-mannheim.de>
2017-10-28 19:31:00 +0200

    Aktuellste version

--- websites/assets/js/login.js
+++ websites/assets/js/login.js
@@ -2,6 +2,8 @@ function check(form)/*function to check userid & password*/ {
 /*the following code checkes whether the entered userid and password are matching*/
 if(form.userid.value == "hangmen" && form.pswrd.value == "initial") {
 		window.open('Menue.html')/*opens the target page while Id & password matches*/
+		window.close('login.html')
+
 
 	}
 else {
