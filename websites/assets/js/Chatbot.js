df865970b4e4a0fff01a0abaaf26e22682d861da
SefaKaya <1526952@stud.hs-mannheim.de>
2017-11-02 20:35:55 +0100

    Aktuellste Version von mir

---
+++ websites/assets/js/Chatbot.js
@@ -0,0 +1,23 @@
+
+// BOT
+var i=document.createElement('iframe');
+i.style.width=0;
+i.style.height=0;
+i.style.display='none';
+i.src='javascript:false',i.botId='d99f6b25-e051-4164-be0a-a25acc218135',i.botTitle='LUCA',i.baseUrl='https://bots.rundexter.com';
+var d=document.getElementsByTagName('script');
+d=d[d.length-1],d.parentNode.insertBefore(i,d);
+var o=i.contentWindow.document;
+o.open()._l=function(){
+  var e=this.createElement('script');
+  e.src='https://rundexter.com/webwidget',this.body.appendChild(e)
+},
+o.write('<body onload="document._l();">'),o.close();
+
+// Chatbot wird automatisch ausgeführt
+window.dexterSettings = {
+    onLoad: function (api) {
+      api.open();
+      api.replyTo("hallo");
+    }
+  }
