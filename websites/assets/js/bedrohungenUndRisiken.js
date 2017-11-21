0807e5a7351b26c75cab47e203b57a3f335469b8
SefaKaya <1526952@stud.hs-mannheim.de>
2017-11-21 12:19:04 +0100

    latest version 21.11.17

--- websites/assets/js/bedrohungenUndRisiken.js
+++ websites/assets/js/bedrohungenUndRisiken.js
@@ -120,6 +120,38 @@ function getLayers(){
 
 
 
+                  // Extend existing 'alert' dialog
+                  if(!alertify.errorAlert){
+                    //define a new errorAlert base on alert
+                    alertify.dialog('errorAlert',function factory(){
+                      return{
+                              build:function(){
+                                  var errorHeader = '<span class="fa fa-times-circle fa-2x" '
+                                  +    'style="vertical-align:middle;color:#e10000;">'
+                                  + '</span> Achtung! Bedrohungen gefunden';
+                                  this.setHeader(errorHeader);
+                              }
+                          };
+                      },true,'alert');
+                  }
+                  //launch it.
+                  // since this was transient, we can launch another instance at the same time.
+                  alertify
+                      .errorAlert('< div style="text-align= center"> Arbeitsschritt: ' + ' ' + '<b style="color: black">' + tagElements[j].getAttribute('name') + '</b>' + '</div>' +  '<br/><br/>' +
+                          "Bedrohung: " + bedrohungen + '<br/>' +
+                          "Bedrohungs-Beschreibung: " + bedrohungenBeschreibung + '<br/>' +
+                          "Risiken: " + risiken + '<br/>' +
+                          "Risiken-Beschreibung: " + risikenBeschreibung + '<br/>' +
+                          "Maßnahmen: " + maßnahmen + '<br/>' +
+                          "Akteure: " + akteure);
+                          //"<a href='javascript:alertify.errorAlert(\"Another error\");'> Click here </a>");
+
+
+
+
+
+
+
 
                   // Hier werden die ganzen Informationen als Pop-up Fesnter angezeigt
                   /*alert('Arbeitsschritt:'+' ' + tagElements[j].getAttribute('name') + '\n' +
