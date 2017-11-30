85e5b9ae395e54316f0241df908c12f849b2ba13
JannisMD <1611773@stud.hs-mannheim.de>
2017-11-30 15:19:50 +0100

    paar frontend sachen

---
+++ websites/assets/js/arbeitsabläufe.js
@@ -0,0 +1,6 @@
+function popUp2() {
+  alertify.error('Es wurden noch keine Arbeitsabläufe bearbeitet');
+}
+function popUp3() {
+  alertify.error('Es wurden noch keine Arbeitsabläufe hochgeladen');
+}
