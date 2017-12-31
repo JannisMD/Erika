0449b56dc801bd98c4536a73fc1da8f8dfefa805
SefaKaya <1526952@stud.hs-mannheim.de>
2017-12-31 15:29:03 +0100

    Anpassung und Kommentare

--- websites/assets/js/Timer.js
+++ websites/assets/js/Timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 25, 2017 00:00:00").getTime();
+var countDownDate = new Date("Jan 18, 2018 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
